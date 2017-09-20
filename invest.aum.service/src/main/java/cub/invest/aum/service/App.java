9a3f81c15c927506154cb7e034bbca11bc8ce1c8
NT48810 <NT48810@cathaybk.com.tw>
2017-09-20 11:02:05 +0800

    20170920 add spring boot,log,resources

--- invest.aum.service/src/main/java/cub/invest/aum/service/App.java
+++ invest.aum.service/src/main/java/cub/invest/aum/service/App.java
@@ -1,13 +1,22 @@
 package cub.invest.aum.service;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	/**
+	 * 程式執行起點
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+
+	}
 }
